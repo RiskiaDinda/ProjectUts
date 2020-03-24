871ae734b655b85aa0f6cf2ca50942c639bd45a6
RiskiaDinda <riskiaramadina35@gmail.com>
2020-03-24 12:54:22 +0700

    Membuat splash screen, membuat layout, dan menambahkan activity untuk insert

--- app/src/main/java/com/example/projectuts/DiaryActivity.java
+++ app/src/main/java/com/example/projectuts/DiaryActivity.java
@@ -1,14 +1,27 @@
 package com.example.projectuts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.projectuts.adapters.DiaryAdapters;
+
 public class DiaryActivity extends AppCompatActivity {
+
+    public static final String DIARY_KEY = "DIARY";
+    public static final String INDEX_KEY = "INDEX";
+    public static final int INSERT_REQUEST = 1;
+    public static final int UPDATE_REQUEST = 2;
+
+    private RecyclerView diaryView;
+    private DiaryAdapters adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        diaryView = findViewById(R.id.rv_diary);
     }
 }
--- app/src/main/java/com/example/projectuts/MainActivity.java
+++ app/src/main/java/com/example/projectuts/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.projectuts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageLogo;
+    TextView textName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageLogo = findViewById(R.id.app_logo);
+        textName = findViewById(R.id.app_name);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, DiaryActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
     }
 }
